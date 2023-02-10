51753b96b0478d43c600e81cdda168d9329d7f6a
GabKid <89548086+gabkidner@users.noreply.github.com>
2023-02-09 18:00:07 -0700

    Delete Capstone.py

--- Capstone.py
+++
@@ -1,7 +0,0 @@
-stones = 20;
-while stones != 0:
-    player = '2'
-    input = int(input(f'It is player {player}\'s turn\nHow many stones will you take?\n'))
-    while input > 3:
-        input = int(input('Invalid input, try again\n'))
-    stones -= input
